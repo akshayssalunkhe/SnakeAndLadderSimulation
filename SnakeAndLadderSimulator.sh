fc5f7bf20d51039b89d6c952331f810e7a4bfab4
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-21 12:20:35 +0530

    ADDED:Variable And Function To Generate Dice Number

--- SnakeAndLadderSimulator.sh
+++ SnakeAndLadderSimulator.sh
@@ -6,3 +6,15 @@ echo Welcome To Snake And Ladder Simulation Problem
 #CONSTANTS
 NUMBER_OF_PLAYER=1;
 STARTING_POSITION=0;
+
+#VARIABLE
+resultNumber=0;
+
+#FUNCTION TO GENERATE ROLL DICE NUMBER
+function rollDice() {
+	resultNumber=$(( $(($RANDOM%6))+1 ))
+	echo $resultNumber
+}
+
+#CALLING FUNCTION TO GET NUMBER
+rollDice
