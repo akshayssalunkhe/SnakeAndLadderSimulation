d4bbb954a0b3f760e20f03e9b722afd803f7ec2e
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-21 15:37:08 +0530

    ADDED:Function To Check Options Snake Ladder And No Play

--- SnakeAndLadderSimulator.sh
+++ SnakeAndLadderSimulator.sh
@@ -6,14 +6,36 @@ echo Welcome To Snake And Ladder Simulation Problem
 #CONSTANTS
 NUMBER_OF_PLAYER=1;
 STARTING_POSITION=0;
+SNAKE=0;
+LADDER=1;
+NO_PLAY=2;
 
-#VARIABLE
+#VARIABLES
 resultNumber=0;
+playerPosition=0;
+option=0;
+
+#FUNCTION TO GENERATE THE OPTION AND GIVE POSITION ACCORDINGLY
+function snakeLadderHold() {
+	case $((RANDOM%3)) in
+		$SNAKE)
+			playerPosition=$(($playerPosition-$resultNumber))
+					;;
+		$LADDER)
+			playerPosition=$(($playerPosition+$resultNumber))
+					;;
+		$NO_PLAY)
+			playerPosition=$playerPosition
+					;;
+	esac
+	echo $playerPosition
+}
 
 #FUNCTION TO GENERATE ROLL DICE NUMBER
 function rollDice() {
 	resultNumber=$(( $(($RANDOM%6))+1 ))
 	echo $resultNumber
+	snakeLadderHold
 }
 
 #CALLING FUNCTION TO GET NUMBER
